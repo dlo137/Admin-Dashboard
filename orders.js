e487385f4a82f69cf832b61f744423677f253586
dlo137 <dangelo.watson1212@gmail.com>
2022-09-26 18:27:08 -0400

    5th commit

---
+++ orders.js
@@ -0,0 +1,52 @@
+const Orders = [
+    {
+        productName: 'Make Waves T-Shirt (1)',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Nightmare Factory Shirts (100)',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+
+    {
+        productName: 'Marios Work T-Shirts (25)',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'HS Football Shirts (13)',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+    {
+        productName: 'Kobe Bootleg Shirt (22)',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+    {
+        productName: "M&M's Cafe Shirts (10)",
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        productName: 'Hippo Red Angel(111)',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+
+]
